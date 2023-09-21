857aeb826485fd110d5a84cc0dfa2dd658c03afb
Malik <malikemiroglu92@gmail.com>
2023-09-21 18:42:58 +0300

    HorizontalScrollBar.jsx, BodyPart.jsx ve SearchExercises.jsx tamamlandı

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -5,9 +5,8 @@ import { exerciseOptions, fetchData } from '../utils/fetchData'
 import HorizontalScrollbar from './HorizontalScrollbar'
 
 
-function SearchExercises() {
+function SearchExercises({ setExercises, bodyPart, setBodyPart}) {
   const [search, setSearch] = useState('')
-  const [exercises, setExercises] = useState([])
   const [bodyParts, setBodyParts] = useState([])
 
   useEffect(() => {
@@ -83,7 +82,11 @@ function SearchExercises() {
       </Box>
 
       <Box sx={{ position:'relative', width:'100%', p:'20px'}}>
-          <HorizontalScrollbar data={bodyParts}/>
+        <HorizontalScrollbar 
+          data={bodyParts} 
+          bodyPart={bodyPart} 
+          setBodyPart={setBodyPart}
+        />
       </Box>
     </Stack>
   )
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,10 +1,10 @@
-const REACT_APP_RAPID_API_KEY = import.meta.env.REACT_APP_RAPID_API_KEY;
+const VITE_REACT_APP_RAPID_API_KEY = import.meta.env.VITE_REACT_APP_RAPID_API_KEY;
 
 export const exerciseOptions = {
     method: 'GET',
     headers: {
         'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-        'X-RapidAPI-Key': "848c7cfb79msh495b98dc6997eb8p1fe36bjsn1f38a2abdbfe"
+        'X-RapidAPI-Key': VITE_REACT_APP_RAPID_API_KEY
     }
 }
 
