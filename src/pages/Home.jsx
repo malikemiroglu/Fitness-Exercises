67e7c6ff5d385684a0ad2927ab5026485eaa3c10
Malik <malikemiroglu92@gmail.com>
2023-09-22 05:33:10 +0300

    Exercise.jsx hazırlandı. ExerciseCard.jsx oluşturulacak.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,9 +18,9 @@ function Home() {
         setBodyPart={setBodyPart}
       />
       <Exercises 
+        exercises={exercises}
         setExercises={setExercises} 
         bodyPart={bodyPart}
-        setBodyPart={setBodyPart}
       />
     </Box>
   )
