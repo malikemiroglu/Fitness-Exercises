4599218ddbaf3791d54336d8ee89e2ac17210fe4
Malik <malikemiroglu92@gmail.com>
2023-09-23 19:29:00 +0300

    Footer.jsx tamamlandi.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Box, Typography, Stack } from '@mui/material'
 
-function Footer() {
+import Logo from '../assets/images/Logo-1.png'
+
+const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <Box mt="80px" bgcolor="#fff3f4">
+      <Stack gap="40px" alignItems="center" px="40px" pt="24px">
+        <img src={Logo} alt="logo" width="200px" height="40px" />
+        <Typography variant="h5" mt="20px" pb="40px">
+          Made with ❤️ by JavaScript Mastery
+        </Typography>
+      </Stack>
+    </Box>
   )
 }
 
