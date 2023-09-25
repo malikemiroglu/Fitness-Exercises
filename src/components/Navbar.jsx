b134272f8b1a29e0b051498fa4cee021d2469c9b
Malik <malikemiroglu92@gmail.com>
2023-09-25 20:02:14 +0300

    Logo public'e alındı

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { Stack } from '@mui/material'
 
-import Logo from '../assets/images/logo.png'
+// import Logo from '/logo.svg'
 
 function Navbar() {
   return (
     <Stack direction="row" justifyContent="space-around" sx={{ gap: {sm: '122px', xs: '40px'}, mt:{ sm: '32px', xs: '20px'}, justifyContent:'none'}} px="20px">
       <Link to="/">
-        <img src={Logo} alt="logo" style={{width:'48px', height:'48px', margin:'0 20px'}}/>
+        <img src="/Logo.png" alt="logo" style={{width:'48px', height:'48px', margin:'0 20px'}}/>
       </Link>
       <Stack
         direction="row"
